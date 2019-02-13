0c76f825cbd4b01bc4ba4d6f81be1a4eaad41881
Gina Yang <ginayang@usc.edu>
2019-02-13 02:28:57 -0800

    Added PA files

---
+++ DynamicProgrammingAssignment/grid.cpp
@@ -0,0 +1,44 @@
+/*
+	CSCI 270 Fall 2018
+	Programming Assignment
+	Name   :
+	Email  :
+	USC ID :
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+//	Feel free to include any other C++ standard library if necessary.
+
+using namespace std;
+//	You can add any custom classes / helper functions here.
+
+int solve(int N, vector<vector<string> > G) {
+/*
+	Please complete this function.
+	N: Number of rows / columns of the grid.
+	G: A 2d vector (N*N) indicating the grid.
+		G[0][0] is the top left corner
+		G[N-1][N-1] is the bottom right corner
+	Return: the minimum life Brain needs to complete his task.
+*/
+}
+
+//	The main function reads the input and outputs your answer.
+//	Please do not modify it. (Assume the input is well-formed;
+//	You don't need to do any error-check)
+int main(int argc, char **argv) {
+	int N;
+	vector<vector<string> > G;
+	cin >> N;
+	G.resize(N);
+	for (int i=0; i<N; i++) {
+		for (int j=0; j<N; j++) {
+			string tmp;
+			cin >> tmp;
+			G[i].push_back(tmp);
+		}
+	}
+	cout << solve(N, G) << endl;
+	return 0;
+}
